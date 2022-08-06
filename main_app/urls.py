71f9dac31f14b3c7f235eab1d0f297119301d77c
Kathy White <kawhite8503@gmail.com>
2022-08-06 15:27:41 -0500

    finish part 5

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
   path('snakes/create/', views.SnakeCreate.as_view(), name="snakes_create"),
   path('snakes/<int:pk>/update/', views.SnakeUpdate.as_view(), name='snakes_update'),
   path('snakes/<int:pk>/delete/', views.SnakeDelete.as_view(), name='snakes_delete'),
-  path('snakes/<int:pk>/add_feeding', views.add_feeding, name='add_feeding'),
+  path('snakes/<int:snake_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+  path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+  path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+  path('toys/', views.ToyList.as_view(), name='toys_index'),
+  path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+  path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
 ]
