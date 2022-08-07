66566eb0a7db538090d3e05192feff8c3e23f747
Kathy White <kawhite8503@gmail.com>
2022-08-07 11:26:44 -0500

    finish snake collector

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('snakes/', views.snakes_index, name='snakes_index'),
   path('snakes/<int:snake_id>/', views.snakes_detail, name='snakes_detail'),
@@ -15,5 +15,6 @@ urlpatterns = [
   path('toys/', views.ToyList.as_view(), name='toys_index'),
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
   path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
-  path('snakes/<int:snake_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy')
+  path('snakes/<int:snake_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Snake, Toy
 from .forms import FeedingForm
 
 # Create your views here.
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def snakes_index(request):
-  snakes = Snake.objects.all()
+  snakes = Snake.objects.filter(user=request.user)
   return render(request, 'snakes/index.html', { 'snakes': snakes})
 
+@login_required
 def snakes_detail(request, snake_id):
   snake = Snake.objects.get(id=snake_id)
   toys_snake_doesnt_have = Toy.objects.exclude(id__in = snake.toys.all().values_list('id'))
@@ -22,19 +29,24 @@ def snakes_detail(request, snake_id):
   return render(request, 'snakes/detail.html', {
     'snake': snake, 'feeding_form': feeding_form, 'toys': toys_snake_doesnt_have })
 
-class SnakeCreate(CreateView):
+class SnakeCreate(LoginRequiredMixin, CreateView):
   model = Snake
   fields = ['name', 'breed', 'description', 'age']
   success_url = '/snakes/'
+    
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
 
-class SnakeUpdate(UpdateView):
+class SnakeUpdate(LoginRequiredMixin, UpdateView):
   model = Snake
   fields = ['breed', 'description', 'age']
 
-class SnakeDelete(DeleteView):
+class SnakeDelete(LoginRequiredMixin, DeleteView):
   model = Snake
   success_url = '/snakes/'
 
+@login_required
 def add_feeding(request, snake_id):
   form = FeedingForm(request.POST)
   if form.is_valid():
@@ -43,24 +55,39 @@ def add_feeding(request, snake_id):
     new_feeding.save()
   return redirect('snakes_detail', snake_id=snake_id)
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
   model = Toy
   fields = '__all__'
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
   model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
   model = Toy
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
   model = Toy
   fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
   model = Toy
   success_url = '/toys/'
 
+@login_required
 def assoc_toy(request, snake_id, toy_id):
   Snake.objects.get(id=snake_id).toys.add(toy_id)
   return redirect('snakes_detail', snake_id=snake_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('cats_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
